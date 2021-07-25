fac76a6b22834026c7935bdae95481ae6c78a28b
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2021-07-26 00:24:02 +0530

    Completed on 25 June 2021

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClOxCZ_x_Aw8FCWfvDgUBukGWSVYaBLfE",
+    authDomain: "netflix-clone-7ca5c.firebaseapp.com",
+    projectId: "netflix-clone-7ca5c",
+    storageBucket: "netflix-clone-7ca5c.appspot.com",
+    messagingSenderId: "538623170369",
+    appId: "1:538623170369:web:184b8441a2e4f4e9def3c0",
+    measurementId: "G-VPNKBTMY4N",
+};
